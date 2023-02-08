22469dbd0ffd5614565b9090fe1fd3c0582f71d6
Aly <el7amrawy@outlook.com>
2023-02-08 19:54:13 +0200

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import config from "./config";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter basename={config.base}>
       <Routes>
         <Route path="/">
-          <Route index element={<>test</>} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,70 @@
+const Home = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left min-w-max">
+          <h1 className="text-5xl font-bold">CHAT APP</h1>
+          <p className="py-6 max-w-3xl">Connect with the world</p>
+        </div>
+        <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                type="text"
+                placeholder="@username"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="user@example.com"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                minLength={8}
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label pb-0">
+                <a
+                  href="#"
+                  className="label-text-alt link link-hover text-primary"
+                >
+                  Already Registered? Login
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-2">
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
